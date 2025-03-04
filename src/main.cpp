7b7861473ac73514a11e4254c0b00ee6c8c57476
DOODLEDO3946 <dinosaurclaw123456@gmail.com>
2025-03-04 19:56:08 +0000

    Should now communicate GPS coords using the radio

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,49 @@
 #include <Arduino.h>
 #include <TinyGPS++.h>
-// Define GPS object
-TinyGPSPlus gps;
+#include <RH_RF95.h>
+#include <RHReliableDatagram.h>
 
 // Define the serial port for GPS communication (e.g., Serial1, Serial2)
 #define GPS_SERIAL Serial1 // You can change to Serial2 or another UART if necessary
 // Define GPS baud rate
 #define GPS_BAUD 9600
+
+// Time to delay before between loops
+#define DELAY_s 0.1
+#define DELAY_ms (DELAY_s * 1000)
+
+// Radio CS pin
+#define CS1 10
+// Particle sensor CS pin
+#define CS2 33
+
+// Define radio reset pin
+#define radio_rst 11
+
+// Define Radio interrupt pin (G0)
+#define radio_int 9
+
+// Radio address for our radio module
+#define FROM_address 0
+#define TO_address 1
+
+
+// For tracking acceleration in altitude
+double last_altitude = 0.0;
+double current_altitude = 0.0;
+
+double last_altitude_updated = 0;
+bool sensors_active = false;
+
+// Define GPS object
+TinyGPSPlus gps;
+
+// Define radio driver object
+RH_RF95 rf95(CS1, radio_int);
+
+// Define another object to send data reliably
+// use methods recvfromAck() and sendtoWait()
+RHReliableDatagram data_transmitter(rf95, FROM_address);
 
 ////////////////////////////
 void setupGPS() {
@@ -22,16 +59,28 @@ void processGPS() {
   while (GPS_SERIAL.available() > 0) {
     gps.encode(GPS_SERIAL.read()); // Feed GPS data to the TinyGPS++ object
 
-    // // Check if there's a new GPS fix (valid location)
-    // if (gps.location.isUpdated()) {
-    //   // Print out the GPS data (latitude, longitude, altitude)
+    // Check if there's a new GPS fix (valid location)
+    if (gps.location.isUpdated()) {
+      last_altitude = current_altitude;
+      current_altitude = gps.altitude.meters();
+
+      double falling_acceleration = (last_altitude - current_altitude) / last_altitude_updated;
+
+      // Activating all required sensors
+      if ((!sensors_active) && (1 < falling_acceleration)) {
+        sensors_active = true;
+        digitalWrite(26, HIGH);
+      }
+      
+      last_altitude_updated = 0;
+      //   // Print out the GPS data (latitude, longitude, altitude)
     //   Serial.print("Latitude= "); 
     //   Serial.print(gps.location.lat(), 6);  // Prints with 6 decimals
     //   Serial.print("Longitude= "); 
     //   Serial.print(gps.location.lng(), 6);  // Prints with 6 decimals
     //   Serial.print("Altitude= "); 
     //   Serial.println(gps.altitude.meters()); // Prints altitude in meters
-    // }
+    }
 
     // // You can add more data like speed, course, date, time, etc.
     // if (gps.speed.isUpdated()) {
@@ -61,29 +110,54 @@ void processGPS() {
 }
 
 void sendvars2PC(){
-  if (Serial.available() > 0) {
-    // structure the data sent as you like, as this is all BASE STATION dictates, not the pc you can just pass everything from the reciever in a pass through manner
-    // sending CANSAT info as json data
-    Serial.print(
-      "{\"Latitude\":"
-      + String(gps.location.lat())
-      + ",\"Longitude\":"
-      + String(gps.location.lng())
-      + ",\"Altitude\":"
-      + String(gps.altitude.meters())
-      + "}"
-    );
+  // sending CANSAT info as json data
+  char json_chars[128];
+
+  sprintf(
+    json_chars,
+    "[%f, %f, %f]",
+    gps.location.lat(), // Degrees
+    gps.location.lng(), // Degrees
+    current_altitude // Metres
+  );
+
+  // The RFM 9x only has a 252 byte buffer, sending in smaller chunks for safety (only useful if the data we are sending is larger than 252 bytes)
+  for (int i = 0; i < sizeof(json_chars); i += 128) {
+    data_transmitter.sendtoWait(((uint8_t*) json_chars), 128, TO_address);
   }
 }
 
 void setup() {
+  // Configuring pins
+  pinMode(25, OUTPUT);
+  pinMode(26, OUTPUT);
+//  pinMode(CS1, OUTPUT);
+  pinMode(CS2, OUTPUT);
+  pinMode(radio_rst, OUTPUT);
+
+  digitalWrite(25, LOW);
+  digitalWrite(26, LOW);
+//  digitalWrite(CS1, LOW);
+  digitalWrite(CS2, LOW);
+  digitalWrite(radio_rst, HIGH);
+
   Serial.begin(115200);  // Start Serial communication with the PC (for debugging)
   setupGPS();  // Initialize the GPS module
+
+  // Manual reset of the radio
+  digitalWrite(radio_rst, LOW);
+  delay(10);
+  digitalWrite(radio_rst, HIGH);
+  delay(10);
+
+  rf95.init();
+  rf95.setFrequency(433.92);
 }
 
 
 void loop() {
-//  processGPS();  // Read and process the GPS data
+  processGPS();  // Read and process the GPS data
   sendvars2PC();
-  delay(2000);  // Wait a bit before reading again
+  delay(DELAY_ms);  // Wait a bit before reading again
+  last_altitude_updated += DELAY_s;
 }
